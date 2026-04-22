7b1b76ef22af2eb196ec1de741d12a62a12c3787
erzan12 <do.earljan@gmail.com>
2026-04-22 15:38:20 +0800

    config(database): added backup database for pgsql

--- config/database.php
+++ config/database.php
@@ -62,7 +62,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-        'mysql_backup' => [
+        'pgsql_back' => [
             'driver' => 'mysql',
             'host' => env('DB_BACKUP_HOST', '127.0.0.1'),
             'port' => env('DB_BACKUP_PORT', '3306'),
@@ -82,13 +82,26 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'ltpms'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', 'postgres'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
+            'sslmode' => 'prefer',
+        ],
+
+        'pgsql_backup' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_BACKUP_HOST', '127.0.0.1'),
+            'port' => env('DB_BACKUP_PORT', '5432'),
+            'database' => env('DB_BACKUP_DATABASE', 'ltpms_backup'),
+            'username' => env('DB_BACKUP_USERNAME', 'postgres'),
+            'password' => env('DB_BACKUP_PASSWORD', 'postgres'),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
             'sslmode' => 'prefer',
         ],
 
